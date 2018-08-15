efca9277fdd86a04b2bba960c23ab4a8be590f69
Yasser Corrales Morales <yasser.corrales.morales@cern.ch>
2018-08-15 20:30:18 +0200

    Change path of git installed

--- bash/add_files/git_ps1.sh
+++ bash/add_files/git_ps1.sh
@@ -1,5 +1,5 @@
 # this is specific to the location of the current version of git, installed by homebrew
-git_prompt_f=/usr/local/Cellar/git/2.13.0/etc/bash_completion.d/git-prompt.sh
+git_prompt_f=/Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
 
 if test -f $git_prompt_f; then
   source $git_prompt_f
