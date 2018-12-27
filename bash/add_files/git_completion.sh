c222dbfc4b86842070ab4e658b552417229d65d5
Yasser Corrales Morales <yasser.corrales.morales@cern.ch>
2018-12-27 15:28:19 +0100

    Fix path to git bash files

--- bash/add_files/git_completion.sh
+++ bash/add_files/git_completion.sh
@@ -1,6 +1,5 @@
 # this is specific to the location of the current version of git, installed by homebrew
-#completion=/usr/local/Cellar/git/2.13.0/etc/bash_completion.d/git-completion.bash
-completion=/Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash
+completion=/usr/local/Cellar/git/2.20.1/etc/bash_completion.d/git-completion.bash
 if test -f $completion; then
   source $completion
 fi
--- bash/add_files/git_ps1.sh
+++ bash/add_files/git_ps1.sh
@@ -1,6 +1,5 @@
 # this is specific to the location of the current version of git, installed by homebrew
-git_prompt_f=/Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
-
+git_prompt_f=/usr/local/Cellar/git/2.20.1/etc/bash_completion.d/git-prompt.sh
 if test -f $git_prompt_f; then
   source $git_prompt_f
 fi
