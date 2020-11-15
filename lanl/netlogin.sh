66dbeebb9276ddc0206ef38f4c82c5b2102fd143
Yasser Corrales Morales <yasser.corrales.morales@cern.ch>
2020-11-15 10:48:54 -0700

    Update lanl netlogin

--- lanl/netlogin.sh
+++ lanl/netlogin.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget https://netlogin.lanl.gov/dana-na/auth/login.cgi
---post-data='tz_offset=-420&username=329869&password=tE0rr)pains&realm=Visitors&btnSubmit=Sign+In' -O -| grep Welcome
+wget https://netlogin.lanl.gov/dana-na/auth/login.cgi --post-data='tz_offset=-420&username=329869&password=tE0rr)pains&realm=Visitors&btnSubmit=Sign+In' -O -| grep Welcome
