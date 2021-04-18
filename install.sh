86c997820cdfa2c622559194694c25cc409474f3
Yasser Corrales Morales <yasser.corrales.morales@cern.ch>
2021-04-17 21:23:27 -0600

    Improve install links

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@
 
   function Main()
   {
-    DOTFILES=${DOTFILES:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"}
+    DOTFILES=${DOTFILES:-"$( cd "$( dirname $(readlink -f "${BASH_SOURCE[0]}") && pwd )" > /dev/null 2>&1 && pwd )"}
 
     if test $OSTYPE = "darwin"*; then
       OS="osx_x86-64"
@@ -24,13 +24,18 @@
     fi
 
     # sourcing script to create the symbolic link
-    files=( "shell/add_files/util.shell" "install/link.sh" )
+    files=( "install/link.sh $1" )
     for file in ${files[@]}; do
       FILE_PATH=${DOTFILES}/$file
-      [ -f ${FILE_PATH} ] && source ${FILE_PATH}
+      [ -f ${FILE_PATH} ] && source "$FILE_PATH"
     done
     [ $OS = "osx_x86-64" ] && _config_myMac || :
   }
+
+  if [ $# -gt 1 ]; then
+    echo "ERROR $(basename $0) accepts at max one option "
+    exit -1
+  fi
 
   Main $@
 )
--- install/link.sh
+++ install/link.sh
@@ -7,25 +7,42 @@ pinfo "" > /dev/null 2>&1 || source ${DOTFILES}/shell/add_files/utils.shell
 #function for symlink
 do_symlink()
 {
-  local __dir="$1" #".( basename $1 )"
-  local __linkables=$2
-  local __ext=${3:-''}
+  local __source=$1
+  local __target=$2
+
+  [[ -e $__target && -z "$force" ]] &&\
+  { perror "~${__target#HOME} already exists... Skipping.";} || \
+  { pinfo  "Creating symlink for $__source $__target"; ln -sfn $__source $__target; }
+}
+
+
+prep_symlink()
+{
+  local __dir=$1
+  local __prefix=${__dir:+$__dir/}$2 #".( basename $1 )"
+  local __linkables=$3
+  local __ext=${4:-''}
+
   plog "\n Creating symlinks"
   plog "================================"
+
   [ ! -d $HOME/$__dir ] && \
     { pdebug "Creating ~/$__dir"; mkdir -p $HOME/$__dir; }
-  for __link in $__linkables; do
-  __target="$HOME/$__dir$( basename $__link $__ext )"
-  [ -e $__target ] && \
-  { perror "~${__target#HOME} already exists... Skipping.";} || \
-  { pinfo  "Creating symlink for $__link"; ln -sfn $__link $__target; }
+
+  for __src in $__linkables; do
+   local __target="$HOME/$__prefix$( basename $__src $__ext )"
+
+    do_symlink $__src $__target
   done
 }
 
-do_symlink '.' "$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )" '.symlink'
-do_symlink ".ssh/" "$DOTFILES/ssh/*"
-do_symlink ".config/" "$DOTFILES/config/*"
-do_symlink '' "$DOTFILES/root/rootlogon.C"
+[[ $1 == "-f" ]] && force=1
+# do_symlink dir prefix links_list [force]
+prep_symlink ''        '.' "$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )" '.symlink'
+prep_symlink '.ssh'    ''  "$DOTFILES/ssh/*"
+prep_symlink '.config' ''  "$DOTFILES/config/*"
+prep_symlink ''        ''  "$DOTFILES/root/rootlogon.C"
+
 # create vim symlinks
 # As I have moved off of vim as my full time editor in favor of neovim,
 # I feel it doesn't make sense to leave my vimrc intact in the dotfiles repo
@@ -40,8 +57,6 @@ VIMFILES=( "$HOME/.vim:$DOTFILES/config/nvim" \
 for __file in "${VIMFILES[@]}"; do
   KEY=${__file%%:*}
   VALUE=${__file#*:}
-  [ -e ${KEY} ] && \
-  { perror "${KEY} already exists... skipping."; } || \
-  { pinfo  "Creating symlink for $KEY";  ln -sfn ${VALUE} ${KEY}; }
+  do_symlink $VALUE $KEY
 done
 )
